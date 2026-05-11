6fcf854577319519a3dbc8afd23b1fec3cdba19e
xiaolongatom <xiaolongatom@gmail.com>
2026-05-11 12:01:40 +0800

    LED0灯1Hz闪烁

--- examples/tbox/src/main.rs
+++ examples/tbox/src/main.rs
@@ -1,15 +1,19 @@
 #![no_main]
 #![no_std]
 
+use ariel_os_boards::pins;
+
 use ariel_os::{
-    log::*,
+    gpio::{Level, Output},
     time::Timer,
 };
 
-#[ariel_os::task(autostart)]
-async fn main() {
+#[ariel_os::task(autostart, peripherals)]
+async fn main(peripherals: pins::LedPeripherals) {
+    let mut led0 = Output::new(peripherals.led0, Level::Low);
+
     loop {
-        info!("Hello World!");
-        Timer::after_secs(1).await;
+        led0.toggle();
+        Timer::after_millis(500).await;
     }
 }
--- src/ariel-os-boards/src/st-stm32f427vg.rs
+++ src/ariel-os-boards/src/st-stm32f427vg.rs
@@ -2,7 +2,7 @@
 
 pub mod pins {
     use ariel_os_hal::hal::peripherals;
-    ariel_os_hal::define_peripherals!(LedPeripherals { led0: PB0, });
+    ariel_os_hal::define_peripherals!(LedPeripherals { led0: PE9, });
     ariel_os_hal::define_peripherals!(ButtonPeripherals { button0: PC13, });
 }
 #[allow(unused_variables)]
